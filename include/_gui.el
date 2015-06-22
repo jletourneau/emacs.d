2267cfe4aa844c32e0d45c0c64a753e8e9c6828c
Jack Letourneau <github@jlet.org>
2015-06-22 15:29:33 -0400

    Enable hl-line-mode for visited files (GUI only).

--- include/_gui.el
+++ include/_gui.el
@@ -1,3 +1,9 @@
 (add-to-list 'default-frame-alist '(font . "fontset-standard"))
 (add-to-list 'default-frame-alist '(width . 100))
 (add-to-list 'default-frame-alist '(height . 50))
+
+(add-hook
+ 'find-file-hook
+ (lambda ()
+   (when (fboundp 'hl-line-mode)
+     (hl-line-mode 1))))
