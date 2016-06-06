23006a349390834cc1b6fb9a66e4e532773d4405
Jack Letourneau <github@jlet.org>
2016-06-06 11:41:49 -0400

    More generous default font size in GUI mode.

--- include/_gui.el
+++ include/_gui.el
@@ -1,4 +1,5 @@
-(add-to-list 'default-frame-alist '(font . "fontset-standard"))
+(add-to-list 'default-frame-alist '(font . "fontset-standard-12"))
+(add-to-list 'default-frame-alist '(line-spacing . 2))
 (add-to-list 'default-frame-alist '(width . 100))
 (add-to-list 'default-frame-alist '(height . 50))
 
