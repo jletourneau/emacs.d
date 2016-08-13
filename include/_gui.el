9f2eeaf6cd1e4ae51095adcc0ae5eaf892368abc
Jack Letourneau <github@jlet.org>
2016-08-13 16:08:55 -0400

    Proper setup of fontset for mostly-Consolas.

--- include/_gui.el
+++ include/_gui.el
@@ -1,4 +1,4 @@
-(add-to-list 'default-frame-alist '(font . "fontset-standard-12"))
+(add-to-list 'default-frame-alist '(font . "fontset-custom"))
 (add-to-list 'default-frame-alist '(line-spacing . 2))
 (add-to-list 'default-frame-alist '(width . 100))
 (add-to-list 'default-frame-alist '(height . 50))
