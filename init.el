434ce665dc0d1b3b082cdaf1ff1e14f761833419
Jack Letourneau <github@jlet.org>
2016-09-20 15:15:24 -0400

    Cruft in init.el to appease Package.el.

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;;
+;; (package-initialize)
+
 (setq package-enable-at-startup nil)
 
 ;; Defer most customization work until basic initialization is complete, so
