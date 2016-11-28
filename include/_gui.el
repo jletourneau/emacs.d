4bdabee15d0e109c3b829017e1928a5a39bcd21d
Jack Letourneau <jletourneau@users.noreply.github.com>
2016-11-28 16:12:29 -0500

    Bind Opt/Cmd to Meta (Control is just Control).

--- include/_gui.el
+++ include/_gui.el
@@ -3,6 +3,11 @@
 (add-to-list 'default-frame-alist '(width . 100))
 (add-to-list 'default-frame-alist '(height . 50))
 
+(setq
+ mac-control-modifier 'control
+ mac-option-modifier 'meta
+ mac-command-modifier 'meta)
+
 (add-hook
  'find-file-hook
  (lambda ()
