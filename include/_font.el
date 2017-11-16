32b42bf2e0916d53ce3b920ab7bfab8b29ef3ce5
Jack Letourneau <jletourneau@users.noreply.github.com>
2017-11-16 10:51:51 -0500

    Allow specifying multiple fonts for fallback.

--- include/_font.el
+++ include/_font.el
@@ -2,7 +2,8 @@
 ;; Fontset config
 
 ;; Sources:
-;;   - SF Mono: included in newer versions of XCode
+;;   - IBM Plex: https://github.com/IBM/type
+;;   - SF Mono: /Applications/Utilities/Terminal.app/Contents/Resources/Fonts
 ;;   - Consolas: https://aur.archlinux.org/packages/ttf-microsoft-consolas/
 ;;   - Symbola: http://users.teilar.gr/~g1951d/
 ;;   - Menlo: packaged with OS X
@@ -11,13 +12,25 @@
 
 ;; Unicode ranges: http://www.alanwood.net/unicode/unicode_samples.html
 
-(defvar default-fixed-pitch-family "SF Mono")
-(defvar default-variable-pitch-family "Lucida Grande")
+(defvar fixed-pitch-families
+  '("IBM Plex Mono" "SF Mono" "Consolas")
+  "List of preferred fixed-pitch font family names.")
+
+(defvar variable-pitch-families
+  '("IBM Plex Sans" "SF Pro Text" "Lucida Grande")
+  "List of preferred variable-pitch font family names.")
+
+(defun first-available-font (font-list)
+  (seq-find (lambda (f) (not (null (x-list-fonts f)))) font-list))
+
+(setq
+ fixed-pitch-family (first-available-font fixed-pitch-families)
+ variable-pitch-family (first-available-font variable-pitch-families))
 
 (set-face-attribute 'fixed-pitch nil
-                    :family default-fixed-pitch-family)
+                    :family fixed-pitch-family)
 (set-face-attribute 'variable-pitch nil
-                    :family default-variable-pitch-family)
+                    :family variable-pitch-family)
 
 (set-face-attribute 'font-lock-comment-face nil
                     :slant 'italic)
@@ -26,11 +39,11 @@
 
 (create-fontset-from-fontset-spec
  (concat "-*-"
-         default-fixed-pitch-family
+         fixed-pitch-family
          "-normal-normal-normal-*-12-*-*-*-m-0-fontset-custom"))
 
 (setq
- fs-default       (font-spec :family default-fixed-pitch-family)
+ fs-default       (font-spec :family fixed-pitch-family)
  fs-symbola       (font-spec :family "Symbola")
  fs-courier       (font-spec :family "Courier")
  fs-arial         (font-spec :family "Arial Unicode MS")
