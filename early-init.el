a9f3a12e0745620ff1cfcbe22ab2097b1e0781f3
Jack Letourneau <jletourneau@users.noreply.github.com>
2022-06-27 08:51:47 -0400

    Tweaks to garbage collection settings.

--- early-init.el
+++ early-init.el
@@ -4,10 +4,17 @@
  gc-cons-threshold (* 128 1024 1024)
  gc-cons-percentage 0.5)
 
+;; Garbage collect on startup completion
 (add-hook
  'emacs-startup-hook
  (lambda ()
    (setq
-    gc-cons-threshold (* 20 1024 1024)
-    gc-cons-percentage 0.1)
+    gc-cons-threshold (* 64 1024 1024)
+    gc-cons-percentage 0.2)
    (garbage-collect)))
+
+;; Garbage collect when Emacs loses focus
+(add-hook 'focus-out-hook 'garbage-collect)
+
+;; Garbage collect when Emacs is idle for 5+ seconds
+(run-with-idle-timer 5.0 t 'garbage-collect)
